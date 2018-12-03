04e0009efb83a2858510c88ff45138c49e8bb619
zhangyanan <zhangyn_10@163.com>
2018-12-03 14:17:02 +0800

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Class PagesController
+ * @package App\Http\Controllers
+ */
 class PagesController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function root()
     {
         return view('pages.root');
+    }
+
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')
+        ->name('email_verify_notice');
+    Route::group(['middleware' => 'email_verify'], function () {
+        Route::get('/test', function () {
+           return 'Your email is verified';
+        });
+    });
+});
