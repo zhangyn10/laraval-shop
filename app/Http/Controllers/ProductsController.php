ee7d20a6db9842b92b5d21384f582647570f8cb1
zhangyanan <zhangyn_10@163.com>
2018-12-10 22:22:42 +0800

    用户界面-商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,8 +14,42 @@ class ProductsController extends Controller
      */
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        // 创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
+        // 判断是否有提交search参数，如果有就赋值给 $search 变量
+        // search 用来模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+            // 模糊搜索商品标题、商品详情、SKU 标题、SKU 描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                      ->orWhere('description', 'like', $like)
+                      ->orWhereHas('skus', function ($query) use ($like) {
+                          $query->where('title', 'like', $like)
+                                ->orWhere('description', 'like', $like);
+                      });
+            });
+        }
 
-        return view('products.index', ['products' => $products]);
+        // 是否有提交 order 参数
+        if ($order = $request->input('order', '')) {
+            // 是否以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    //  根据传入的排序值来构造排序函数
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order,
+            ],
+        ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- csrf_token -->
-    <meta name="csrf_token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'shop') - shop</title>
     <!-- 样式 -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
