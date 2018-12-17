36035941e2a34932338b6cb0d7bf6681cc03a82f
zhangyanan <zhangyn_10@163.com>
2018-12-17 19:11:58 +0800

    商品加入购物车

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -101,6 +101,36 @@
                             })
                     });
             });
+
+            // 添加加入购物车事件
+            $('.btn-add-to-card').click(function () {
+                // 请求加入购物车接口
+                axios.post('{{ route('cart.add') }}', {
+                    sku_id: $('label.active input[name=skus]').val(),
+                    amount: $('.cart_amount input').val(),
+                })
+                    .then(function () { // 请求成功执行此回调
+                        swal('加入购物车成功', '', 'success');
+                    }, function (error) {//请求失败执行此回调
+                        if (error.response.status === 401) {
+                            // 401表示未登录
+                            swal('请先登录', '', 'error');
+                        } else if (error.response.status === 422) {
+                            // 422代表输入校验失败
+                            var html = '<div>';
+                            _.each(error.response.data.errors, function (errors) {
+                                _.each(errors, function (error) {
+                                    html += error + '<br/>';
+                                })
+                            });
+                            html += '</div>';
+                            swal({content: $(html)[0], icon: 'error'});
+                        } else {
+                            // 其他情况
+                            swal('系统错误', '', 'error');
+                        }
+                    });
+            });
         });
     </script>
 @endsection
