1b083539b0b410281bd6ea2899ed09623dee2bc1
zhangyanan <zhangyn_10@163.com>
2019-02-15 17:41:05 +0800

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,8 @@ class AppServiceProvider extends ServiceProvider
         // 往容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
+            $config['notify_url'] = route('payment.alipay.notify');
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前环境是否为线上
             if (app()->environment() !== 'production') {
                 $config['mode'] = 'dev';
